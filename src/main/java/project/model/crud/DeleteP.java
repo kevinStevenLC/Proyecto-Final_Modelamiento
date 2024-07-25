0da27d915d5c449881e921bddb834b81f7414128
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-24 19:45:47 -0500

    Finalizando los metodos crud

--- src/main/java/project/model/crud/DeleteP.java
+++ src/main/java/project/model/crud/DeleteP.java
@@ -1,5 +1,43 @@
 package project.model.crud;
 
-public class DeleteP {
+import project.model.DbConexion;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteP {
+	Connection connection;
+
+	public DeleteP() {
+		try {
+			this.connection = DbConexion.conexion_db();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void eliminarProducto(int idProducto) {
+		String query = "DELETE FROM Producto WHERE idProducto = ?";
+
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+			preparedStatement.setInt(1, idProducto);
+
+			int rowsDeleted = preparedStatement.executeUpdate();
+			if (rowsDeleted > 0) {
+				System.out.println("Producto eliminado exitosamente!");
+			}
+
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		DeleteP deleteP = new DeleteP();
+		deleteP.eliminarProducto(78); // Eliminar producto con ID 1
+	}
 }
--- src/main/java/project/model/crud/ModifyP.java
+++ src/main/java/project/model/crud/ModifyP.java
@@ -1,5 +1,52 @@
 package project.model.crud;
 
+import project.model.DbConexion;
+import project.model.Producto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ModifyP {
 
+	Connection connection;
+
+	public ModifyP() {
+		try {
+			this.connection = DbConexion.conexion_db();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void modificarProducto(Producto producto) {
+		String query = "UPDATE Producto SET descripcion = ?, precio = ?, stock = ?, id_Categoria = ? WHERE idProducto = ?";
+
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+			preparedStatement.setString(1, producto.getDescripcionP());
+			preparedStatement.setFloat(2, producto.getPrecio());
+			preparedStatement.setInt(3, producto.getInv());
+			preparedStatement.setInt(4, producto.getCategoria());
+			preparedStatement.setInt(5, producto.getIdProducto());
+
+			int rowsUpdated = preparedStatement.executeUpdate();
+			if (rowsUpdated > 0) {
+				System.out.println("Producto actualizado exitosamente!");
+			}
+
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		ModifyP modifyP = new ModifyP();
+		Producto producto = new Producto("Producto Creado 3 (modificacion)", 150.0f, 30, 4);
+		producto.setIdProducto(93);
+		modifyP.modificarProducto(producto);
+
+	}
 }
